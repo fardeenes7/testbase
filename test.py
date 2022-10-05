5fd488f118d97095a17a4156eb935b6869142a08
Fardeen Ehsan <fardeen.es7@gmail.com>
2022-10-05 06:07:54 +0600

    first commit

---
+++ test.py
@@ -0,0 +1,17 @@
+
+
+def checknum(a):
+    if a==0:
+        return 'zero'
+    elif a>0:
+        return 'positive'
+    else:
+        return 'negative'
+
+
+print(checknum(5))
+
+print(checknum(-5))
+
+
+
